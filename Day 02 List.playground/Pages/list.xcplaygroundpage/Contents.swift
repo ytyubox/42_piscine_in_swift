833177f2fb578b49bec34f7172f0ac93f212ec34
inkinyu <inkinyu@gmail.com>
2018-12-23 20:35:34 +0800

    [day start] day 02 list

--- Day 02 List.playground/Pages/list.xcplaygroundpage/Contents.swift
+++ Day 02 List.playground/Pages/list.xcplaygroundpage/Contents.swift
@@ -1,6 +1,16 @@
 //: [Previous](@previous)
 
-import Foundation
+/*:
+# Day 02
+- Exercise 00 : ft_print_alphabet
+- Exercise 01 : ft_print_reverse_alphabet
+- Exercise 02 : ft_print_numbers
+- Exercise 03: ft_is_negative
+- Exercise 04 : ft_print_comb
+- Exercise 05 : ft_print_comb2
+- Exercise 06 : ft_putnbr
+- Exercise 07 : ft_print_combn
+*/
 
 
 
