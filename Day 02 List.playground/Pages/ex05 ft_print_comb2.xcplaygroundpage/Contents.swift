08fdaddbcf468e3e57da41af86c3ac4f17711a6f
inkinyu <inkinyu@gmail.com>
2018-12-23 20:44:58 +0800

    [daily log] on day 05 ex05

---
+++ Day 02 List.playground/Pages/ex05 ft_print_comb2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+# Exercise 05 : ft_print_comb2
+
+Turn-in directory : ex05/
+
+Files to turn in : ft_print_comb2.c
+
+Allowed functions : ft_putchar
+
+Remarks : n/a
+- Create a function that displays all di
+- Here’s the expected output :
+- Here’s how it should be prototyped : void ft_print_comb2(void);
+````
+$>./a.out | cat -e
+00 01, 00 02, 00 03, 00 04, 00 05, ..., 00 99, 01 02, ..., 97 99, 98 99$>
+````
+*/
+
+//: [Next](@next)
