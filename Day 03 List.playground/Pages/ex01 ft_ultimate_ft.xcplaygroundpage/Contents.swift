08aa9d907ba7db804b8db3266dda3b6201d47563
ytyubox <ytyubox@gmail.com>
2018-12-26 17:54:02 +0800

    [ex edit] day03 ex01~03: fix render

--- Day 03 List.playground/Pages/ex01 ft_ultimate_ft.xcplaygroundpage/Contents.swift
+++ Day 03 List.playground/Pages/ex01 ft_ultimate_ft.xcplaygroundpage/Contents.swift
@@ -1,19 +1,22 @@
 //: [Previous](@previous)
 
 import Foundation
+/*:
+#  Exercise 01 : ft_ultimate_ft
 
-//: Exercise 01 : ft_ultimate_ft
 
-//:
-//:
-//: Turn-in directory : ex01/
-//: Files to turn in : ft_ultimate_ft.c
-//: Allowed functions : Nothing
-//: Remarks : n/a
-//: • Create a function that takes a pointer to pointer to pointer to pointer to pointer to pointer to pointer to pointer to pointer to int as a parameter and sets the value "42" to that int.
-//: • Here’s how it should be prototyped : void ft_ultimate_ft(int *********nbr);
 
-var nb1 = 0
+ Turn-in directory : ex01/
+
+ Files to turn in : ft_ultimate_ft.c
+
+ Allowed functions : Nothing
+
+ Remarks : n/a
+ - Create a function that takes a pointer to pointer to pointer to pointer to pointer to pointer to pointer to pointer to pointer to int as a parameter and sets the value "42" to that int.
+ - Here’s how it should be prototyped : void ft_ultimate_ft(int *********nbr);
+*/
+var nbr1 = 0
 
 func ft_ultimate_ft(_ nbr:
 	UnsafeMutablePointer
@@ -40,7 +43,7 @@ func ft_ultimate_ft(_ nbr:
 	nbr.pointee.pointee.pointee.pointee.pointee.pointee.pointee.pointee.pointee  = 42
 }
 
-var ptr1:UnsafeMutablePointer<Int> = withUnsafeMutablePointer(to: &nb1) {return $0}
+var ptr1 = withUnsafeMutablePointer(to: &nbr1) {return $0}
 var ptr2 = withUnsafeMutablePointer(to: &ptr1) {return $0}
 var ptr3 = withUnsafeMutablePointer(to: &ptr2) {return $0}
 var ptr4 = withUnsafeMutablePointer(to: &ptr3) {return $0}
@@ -49,7 +52,8 @@ var ptr6 = withUnsafeMutablePointer(to: &ptr5) {return $0}
 var ptr7 = withUnsafeMutablePointer(to: &ptr6) {return $0}
 var ptr8 = withUnsafeMutablePointer(to: &ptr7) {return $0}
 var ptr9 = withUnsafeMutablePointer(to: &ptr8) {return $0}
+nbr1
 ft_ultimate_ft(ptr9)
-nb1
+nbr1
 
 //: [Next](@next)
--- Day 03 List.playground/Pages/ex02 ft_swap.xcplaygroundpage/Contents.swift
+++ Day 03 List.playground/Pages/ex02 ft_swap.xcplaygroundpage/Contents.swift
@@ -1,18 +1,21 @@
 //: [Previous](@previous)
 
 import Foundation
+/*:
+# Exercise 02 : ft_swap
 
-//: Exercise 02 : ft_swap
 
-//:
-//:
-//: Turn-in directory : ex02/
-//: Files to turn in : ft_swap.c
-//: Allowed functions : Nothing
-//: Remarks : n/a
-//: • Create a function that swaps the value of two integers whose addresses are entered as parameters.
-//: • Here’s how it should be prototyped : void ft_swap(int *a, int *b);
 
+ Turn-in directory : ex02/
+
+Files to turn in : ft_swap.c
+
+Allowed functions : Nothing
+
+Remarks : n/a
+ - Create a function that swaps the value of two integers whose addresses are entered as parameters.
+ - Here’s how it should be prototyped : void ft_swap(int *a, int *b);
+*/
 func ft_swap(_ a:inout Int, _ b:inout Int)
 {
 	a ^= b
