3b60470252e0d9fac10c1adca9707c9ddc7ee195
ytyubox <ytyubox@gmail.com>
2018-12-26 17:52:14 +0800

    [ex edit] day03 ex00~03: fix render

--- Day 03 List.playground/Pages/ex00  ft_ft.xcplaygroundpage/Contents.swift
+++ Day 03 List.playground/Pages/ex00  ft_ft.xcplaygroundpage/Contents.swift
@@ -1,17 +1,20 @@
 //: [Previous](@previous)
 
 import Foundation
+/*:
+# Exercise 00 : ft_ft
 
-//:Exercise 00 : ft_ft
 
-//:
-//:
-//:Turn-in directory : ex00/
-//:Files to turn in : ft_ft.c
-//:Allowed functions : Nothing
-//:Remarks : n/a
-//:• Create a function that takes a pointer to int as a parameter, and sets the value "42" to that int.
-//:• Here’s how it should be prototyped : void ft_ft(int *nbr);
+Turn-in directory : ex00/
+
+Files to turn in : ft_ft.c
+
+Allowed functions : Nothing
+
+Remarks : n/a
+-  Create a function that takes a pointer to int as a parameter, and sets the value "42" to that int.
+-  Here’s how it should be prototyped : void ft_ft(int *nbr);
+*/
 
 func ft_ft(_ nbr: inout Int)
 {
